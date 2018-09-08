ecf4ec6977b177dfe208530801ae53d7308de8be
westim <timothy.k.west@gmail.com>
2018-09-07 21:07:29 -0700

    Create GraphicalSet folder for Assignment 3

---
+++ GraphicalSet/GraphicalSet/Controller/ViewController.swift
@@ -0,0 +1,202 @@
+//
+//  ViewController.swift
+//  Set
+//
+//  Created by Timothy West on 8/26/18.
+//  Copyright © 2018 Tim West. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    private lazy var game = SetGame()
+    
+    private let shapes: [Card.Variant: String] =
+        [.one: "▲",
+         .two: "■",
+         .three: "●"]
+    private let colors: [Card.Variant: UIColor] =
+        [.one: UIColor(red: 1, green: 1, blue: 0, alpha: 1),
+         .two: UIColor(red: 1, green: 0, blue: 1, alpha: 1),
+         .three: UIColor(red: 0, green: 1, blue: 1, alpha: 1)]
+    private let counts: [Card.Variant: Int] =
+        [.one: 1,
+         .two: 2,
+         .three: 3]
+    
+    private var isBoardFull: Bool {
+        return game.dealtCards.count == cardButtons.count
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        for button in cardButtons {
+            button.layer.cornerRadius = 8.0
+            button.titleLabel?.font.withSize(12.0)
+        }
+        createDeal3CardsDisabledText()
+        updateViewFromModel()
+    }
+    
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        guard let index = cardButtons.index(of: sender) else { return }
+        game.selectCard(clickedCardIndex: index)
+        updateViewFromModel()
+    }
+    
+    @IBAction func touchHint(_ sender: UIButton) {
+        guard let set = game.findMatchingSet() else { return }
+        for card in set {
+            let index = game.dealtCards.index(of: card)
+            let button = cardButtons[index!]
+            button.layer.borderWidth = 3.0
+            button.layer.borderColor = UIColor(red: 0, green: 1, blue: 0, alpha: 1).cgColor
+        }
+    }
+    
+    @IBOutlet private var scoreLabel: UILabel!
+    @IBOutlet private var cardsLeftLabel: UILabel!
+    
+    @IBOutlet private var newGameButton: UIButton!
+    @IBOutlet private var deal3CardsButton: UIButton!
+    @IBOutlet private var hintButton: UIButton!
+    
+    /**
+     Changes the Deal 3 Cards button text color to indicate
+     that no more cards can be dealt.
+     */
+    private func createDeal3CardsDisabledText() {
+        let disabledAttributes: [NSAttributedStringKey: Any] = [
+            NSAttributedStringKey.foregroundColor: UIColor(red: 1, green: 0, blue: 0, alpha: 1)
+        ]
+        deal3CardsButton.setAttributedTitle(NSAttributedString(string: deal3CardsButton.titleLabel!.text!, attributes: disabledAttributes), for: .disabled)
+    }
+    
+    @IBAction func startNewGame(_ sender: UIButton) {
+        game.startGame()
+        newGameButton.isHidden = true
+        deal3CardsButton.isHidden = false
+        deal3CardsButton.isEnabled = true
+        updateViewFromModel()
+    }
+    
+    private func endGame() {
+        newGameButton.isHidden = false
+        deal3CardsButton.isHidden = true
+    }
+    
+    @IBAction func deal3Cards(_ sender: UIButton) {
+        game.dealCards()
+        updateViewFromModel()
+    }
+    
+    /**
+     Constructs the card's attributed text based on
+     the card's other attributes.
+     
+     - Parameter card: The card for which to create attributed text.
+     
+     - Returns: Attributed text for the card.
+     */
+    private func getAttributedText(forCard card: Card) -> NSAttributedString? {
+       
+        guard let shape = shapes[card.Attribute1] else { return nil }
+        guard let color = colors[card.Attribute2] else { return nil }
+        guard let count = counts[card.Attribute3] else { return nil }
+        
+        let cardText = String(repeating: shape, count: count)
+        var attributes = [NSAttributedStringKey : Any]()
+        
+        switch card.Attribute4 {
+            case .one:  // outlined
+                attributes[NSAttributedStringKey.strokeWidth] = 10
+                fallthrough
+            case .two:  // solid
+                attributes[NSAttributedStringKey.foregroundColor] = color
+            case .three:  // faded
+                attributes[NSAttributedStringKey.foregroundColor] = color.withAlphaComponent(0.5)
+        }
+        
+        let attributedText = NSAttributedString(string: cardText, attributes: attributes)
+        return attributedText
+    }
+    
+    private func updateScoreLabel() {
+        scoreLabel.text = "Score: \(game.score)"
+    }
+    
+    private func updateCardsLeftLabel() {
+        cardsLeftLabel.text = "Cards Left: \(game.deck.count)"
+    }
+    
+    private func updateViewFromModel() {
+        updateScoreLabel()
+        updateCardsLeftLabel()
+        
+        if game.gameOver {
+            endGame()
+        } else {
+            // Can't deal cards when the board is full or deck is empty
+            deal3CardsButton.isEnabled = !isBoardFull && !game.deck.isEmpty
+        }
+
+        for index in game.dealtCards.indices {
+            enableButton(at: index)
+            cardButtons[index].setAttributedTitle(getAttributedText(forCard: game.dealtCards[index]), for: .normal)
+            
+            if game.selectedCards.contains(game.dealtCards[index]) {
+                addSelectedBorder(to: index)
+                
+                // Add colors to indicate a successful/unsuccessful set
+                if let isSetMatching = game.selectedSetMatches {
+                    if isSetMatching {
+                        cardButtons[index].layer.backgroundColor = UIColor(red: 0, green: 1, blue: 0, alpha: 0.3).cgColor
+                    } else {
+                        cardButtons[index].layer.backgroundColor = UIColor(red: 1, green: 0, blue: 0, alpha: 0.3).cgColor
+                    }
+                }
+            } else {
+                // Clear border color for unselected cards
+                cardButtons[index].layer.borderColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0).cgColor
+            }
+        }
+        hideUnusedButtons()
+    }
+    
+    /**
+     Enables the button at the given index.
+     
+     - Parameter index: The index of the button to enable.
+     */
+    private func enableButton(at index: Int) {
+        cardButtons[index].isEnabled = true
+        cardButtons[index].swapToColor(withDuration: 0.2, toColor: UIColor(red: 0, green: 0, blue: 0, alpha: 1))
+    }
+    
+    /**
+     Identifies all unused buttons on the View and
+     makes the buttons disabled and "invisible".
+     */
+    private func hideUnusedButtons() {
+        let hiddenButtons: [UIButton] = cardButtons.filter { !game.dealtCards.indices.contains(cardButtons.index(of: $0)!) }
+        hiddenButtons.forEach() {
+            $0.swapToColor(withDuration: 0.2, toColor: super.view.backgroundColor!)
+            $0.isEnabled = false
+            $0.layer.borderColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0).cgColor
+            $0.setAttributedTitle(nil, for: .normal) }
+    }
+    
+    /**
+     Creates a highlighted border around the specified button.
+     
+     - Parameter index: The index of the button to highlight.
+     */
+    private func addSelectedBorder(to index: Int) {
+        let button = cardButtons[index]
+        button.layer.borderWidth = 3.0
+        button.layer.borderColor = UIColor(red: 1, green: 1, blue: 0, alpha: 1).cgColor
+    }
+}
---
+++ GraphicalSet/GraphicalSet/Model/SetGame.swift
@@ -0,0 +1,144 @@
+//
+//  SetGame.swift
+//  Set
+//
+//  Created by Timothy West on 8/28/18.
+//  Copyright © 2018 Tim West. All rights reserved.
+//
+
+import Foundation
+
+struct SetGame {
+    
+    var deck = [Card]()
+    private(set) var selectedCards = [Card]()
+    private(set) var dealtCards = [Card]()
+    private(set) var score = 0
+    
+    /// End game state is when there are no dealt cards & the deck is empty.
+    var gameOver: Bool {
+        return dealtCards.isEmpty && deck.isEmpty
+    }
+    
+    /// Determines if the current selected set is a match.
+    /// If current selection isn't a complete set, return `nil`.
+    var selectedSetMatches: Bool? {
+        return try? Card.makesSet(selectedCards)
+    }
+    
+    init() {
+        startGame()
+    }
+    
+    /**
+     Populates the deck with all possible card variations, shuffled randomly.
+     
+     - Returns: Shuffled deck of `Card`
+     */
+    private func createDeck() -> [Card] {
+        var newDeck = [Card]()
+        for attribute1 in Card.Variant.all {
+            for attribute2 in Card.Variant.all {
+                for attribute3 in Card.Variant.all {
+                    for attribute4 in Card.Variant.all {
+                        newDeck.append(Card(Att1: attribute1, Att2: attribute2, Att3: attribute3, Att4: attribute4))
+                    }
+                }
+            }
+        }
+        newDeck.shuffle()
+        return newDeck
+    }
+    
+    /**
+     Searches the board for a set and returns the
+     first found matching set.
+     
+     - Returns: Matching set of 3 cards or nil if no matching set is present
+     */
+    func findMatchingSet() -> [Card]? {
+        if dealtCards.count < 3 {
+            return nil
+        }
+        for card1 in dealtCards {
+            for card2 in dealtCards {
+                for card3 in dealtCards {
+                    if (card1 != card2 && card2 != card3 && card1 != card3) {
+                        if let matches = try? Card.makesSet([card1, card2, card3]), matches {
+                            return [card1, card2, card3]
+                        }
+                    }
+                }
+            }
+        }
+        return nil
+    }
+    
+    /**
+     Deals the first 12 cards from the deck & resets the score.
+     */
+    mutating func startGame() {
+        deck = createDeck()
+        dealtCards = Array(deck[0..<12])
+        deck.removeSubArray(subarray: dealtCards)
+        score = 0
+    }
+    
+    /**
+     Deals 3 cards from the deck. If the selected set
+     matches, replace the selected set. Otherwise, add the
+     cards to the collection of dealt cards.
+     */
+    mutating func dealCards() {
+        let numberOfCardsToDeal = deck.count < 3 ? deck.count : 3
+        let dealCards = Array(deck[0..<numberOfCardsToDeal])
+        
+        if let isMatch = selectedSetMatches, isMatch {  // Replace selected, matching cards
+            replaceSelectedCards(using: dealCards)
+        } else {  // Add cards to the game
+            dealtCards.append(contentsOf: dealCards)
+        }
+        deck.removeSubArray(subarray: dealCards)
+    }
+    
+    /**
+     Replaces the selected cards with new cards from the deck.
+     If there are not enough cards from the deck, simply remove
+     the dealt card from the game.
+     */
+    mutating private func replaceSelectedCards(using newCards: [Card]) {
+        for selectedCardIndex in selectedCards.indices {
+            guard let dealtCardIndex = dealtCards.index(of: selectedCards[selectedCardIndex]) else { return }
+            if selectedCardIndex < newCards.count {
+                dealtCards[dealtCardIndex] = newCards[selectedCardIndex]
+            } else {
+                dealtCards.remove(at: dealtCardIndex)
+            }
+        }
+    }
+    
+    /**
+     Acts on the clicked `Card` according to the game rules.
+     
+     - Parameter clickedCard: The card that was selected by the player.
+     */
+    mutating func selectCard(clickedCardIndex: Int) {
+        if let index = selectedCards.index(of: dealtCards[clickedCardIndex]), selectedCards.count != 3 {  // Deselect clicked card
+            score -= 1
+            selectedCards.remove(at: index)
+        } else {
+             if let isMatched = selectedSetMatches, !isMatched {  // Deselect unmatched set & select clicked card
+                score -= 5
+                selectedCards.removeAll()
+                selectedCards.append(dealtCards[clickedCardIndex])
+            } else if let isMatched = selectedSetMatches, isMatched {  // Replace matched set
+                score += 3
+                dealCards()
+                selectedCards.removeAll()
+            } else {  // Select clicked card
+                selectedCards.append(dealtCards[clickedCardIndex])
+            }
+        }
+    }
+}
+
